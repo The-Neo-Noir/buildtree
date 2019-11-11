e367176ad00d56da460b71530ee23b933554eeeb
Abhiram Mishra <The-Neo-Noir@users.noreply.github.com>
2019-11-11 21:18:45 +0530

    Update app.js

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -58,52 +58,6 @@ function updateBuildingProcessToolTip() {
 }
 
 
-function setConnected(connected) {
-    $("#connect").prop("disabled", connected);
-    $("#disconnect").prop("disabled", !connected);
-    if (connected) {
-        $("#conversation").show();
-    }
-    else {
-        $("#conversation").hide();
-    }
-    $("#greetings").html("");
-}
-
-function connect() {
-    var socket = new SockJS('/fullduplex');
-    stompClient = Stomp.over(socket);
-    stompClient.connect({}, function (frame) {
-        setConnected(true);
-        console.log('Connected: ' + frame);
-        stompClient.subscribe('/topic/ConsoleLogs', function (greeting) {
-            // showConsoleLog(greeting.body);
-        });
-        stompClient.subscribe('/topic/StatusUpdate', function (stausMsg) {
-            //  var parse = JSON.parse( stausMsg.toString() );
-            animate(JSON.parse(stausMsg.body).id);
-            // AVeryComplecatedSectionTODO(greeting.body);
-        });
-    });
-}
-
-function disconnect() {
-    if (stompClient !== null) {
-        stompClient.disconnect();
-    }
-    setConnected(false);
-    console.log("Disconnected");
-}
-
-function sendName() {
-    stompClient.send("/app/hello", {}, JSON.stringify({'name': 'Abhirms'}));
-}
-
-function showConsoleLog(message) {
-    toastr.info(message);
-
-    $("#greetings").append("<tr><td>" + message + "</td></tr>");
-}
 
 function showHideOutcome(value) {
     if (value === 'tag' || value === 'Any branch') {
@@ -113,18 +67,4 @@ function showHideOutcome(value) {
     }
 
 }
-
-$(function () {
-    $("form").on('submit', function (e) {
-        e.preventDefault();
-    });
-    $("#connect").click(function () {
-        connect();
-    });
-    $("#disconnect").click(function () {
-        disconnect();
-    });
-    $("#send").click(function () {
-        sendName();
-    });
 });
